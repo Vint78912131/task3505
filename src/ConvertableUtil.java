7013df79574c08f02c307a1e8a0da5319d859bb4
v sotnikov <sotnikov@rosplatforma.ru>
2024-03-01 14:59:06 +0300

    01032024commit

---
+++ src/ConvertableUtil.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ConvertableUtil {
+
+    public static <Key, E extends Convertable<Key>> Map<Key, E> convert(List<E> list) {
+        Map<Key, E> result = new HashMap();
+        for (E element : list) {
+            Key key = element.getKey();
+            result.put(key, element);
+        }
+        return result;
+    }
+}
